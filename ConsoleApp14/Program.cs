66eb52d3ce3557186759ffedca665d5ae0962772
Suwath Ch <suwatch@microsoft.com>
2020-08-13 08:35:27 -0700

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Threading;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Running {typeof(string).Assembly.Location}");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
